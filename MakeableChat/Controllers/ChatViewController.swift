48361967e8652c4d312c3950236ff8545dad023f
Kirsten Ivarsen <kirstenivarsen@gmail.com>
2021-02-12 13:26:34 +0100

    nameDialog added, user can change name and id

--- MakeableChat/Controllers/ChatViewController.swift
+++ MakeableChat/Controllers/ChatViewController.swift
@@ -10,16 +10,33 @@ class ChatViewController: JSQMessagesViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Current user information
-        senderId = "1234"
-        senderDisplayName = "Kirsten"
-        
-        setupBubbles()
-        automaticallyScrollsToMostRecentMessage = true
-  
-        // Testdata
+        // Testdata - messages from random users
         DbConstants.dbChats.childByAutoId().setValue(["senderId": "2345", "senderDisplayName": "Bob", "messageContent": "Hello there!"])
-        DbConstants.dbChats.childByAutoId().setValue(["senderId": "3456", "senderDisplayName": "Alice", "messageContent": "Testmessage from another user"])
+        DbConstants.dbChats.childByAutoId().setValue(["senderId": "3456", "senderDisplayName": "Cleo", "messageContent": "Testmessage from another user"])
+        
+        let defaults = UserDefaults.standard
+        
+        // Checks if user already has given ID
+        if let id = defaults.string(forKey: "defaultsId"),
+           let name = defaults.string(forKey: "defaultsName") {
+            senderId = id
+            senderDisplayName = name
+        } else {
+            // If user doesn't have ID, a random ID is generated and nameDialog opens
+            let randomNumber = Int.random(in: 1..<1000)
+            senderId = String(randomNumber)
+            senderDisplayName = ""
+            
+            defaults.set(senderId, forKey: "defaultsId")
+            defaults.synchronize()
+            
+            displayNameDialog()
+        }
+       
+        // If user wants to change name, tap navigationbar
+        title = "Chat: \(senderDisplayName!)"
+        let tap = UITapGestureRecognizer(target: self, action: #selector(displayNameDialog))
+        navigationController?.navigationBar.addGestureRecognizer(tap)
         
         // Sets the avatar viewSize to zero
         inputToolbar.contentView.leftBarButtonItem = nil
@@ -39,6 +56,40 @@ class ChatViewController: JSQMessagesViewController {
                 self.finishReceivingMessage()
             }
         })
+        setupBubbles()
+        automaticallyScrollsToMostRecentMessage = true
+    }
+    
+    // Displays dialog where user enters name. Generates random ID and saves both in userDefaults
+    @objc func displayNameDialog() {
+        let defaults = UserDefaults.standard
+        let alert = UIAlertController(title: "Enter name", message: "Please enter a display name", preferredStyle: .alert)
+        
+        alert.addTextField { nameTextField in
+            if let name = defaults.string(forKey: "defaultsName") {
+                nameTextField.text = name
+                let randomNumber = Int.random(in: 1..<1000)
+                self.senderId = String(randomNumber)        
+                defaults.set(self.senderId, forKey: "defaultsId")
+            } else {
+                nameTextField.text = ""
+            }
+        }
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak self, weak alert] (_) in
+            if let nameTextField = alert?.textFields?[0],
+               !nameTextField.text!.isEmpty {
+                
+                self?.senderDisplayName = nameTextField.text
+                self?.title = "Chat: \(self!.senderDisplayName!)"
+                
+                defaults.set(nameTextField.text, forKey: "defaultsName")
+                defaults.synchronize()
+            }
+        }))
+       
+        automaticallyScrollsToMostRecentMessage = true
+        present(alert, animated: true, completion: nil)
     }
     
     private func setupBubbles() {
@@ -74,9 +125,10 @@ class ChatViewController: JSQMessagesViewController {
     
     // Display senders name in toplabel if senderId different from current user
     override func collectionView(_ collectionView: JSQMessagesCollectionView!, attributedTextForMessageBubbleTopLabelAt indexPath: IndexPath!) -> NSAttributedString! {
-        return messages[indexPath.item].senderId == senderId ? nil : NSAttributedString(string: messages[indexPath.item].senderDisplayName)
+        return NSAttributedString(string: messages[indexPath.item].senderDisplayName)
     }
     
+    // Has to be set in order for toplabel to show
     override func collectionView(_ collectionView: JSQMessagesCollectionView!, layout collectionViewLayout: JSQMessagesCollectionViewFlowLayout!, heightForMessageBubbleTopLabelAt indexPath: IndexPath!) -> CGFloat {
         return messages[indexPath.item].senderId == senderId ? CGFloat(0.0) : kJSQMessagesCollectionViewCellLabelHeightDefault
     }
